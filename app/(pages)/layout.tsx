9f9f16dbb21fc1e77b30f4e600c151fffd5d6acd
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2025-02-15 00:15:01 +0300

    Add Radix UI components for enhanced header functionality; implement dropdown menus, badges, and avatars

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -9,17 +9,16 @@ const PageLayout = ({children}: { children: React.ReactNode }) => {
     const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
     useAuth();
 
-    // authService.isLoggedIn() ? children : <Navigate to="/login" />;
     return (
         <div className="flex h-screen">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
 
             <div className="flex flex-col flex-1">
-                <Header setSidebarOpen={setSidebarOpen}/>
+                <Header setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen}/>
 
                 <main
                     className={`flex-1 overflow-y-auto transition-all duration-300 ${
-                        sidebarOpen ? "ml-72" : "ml-20"
+                        sidebarOpen ? "ml-72" : "ml-0"
                     }`}
                 >
                     <div className="p-6">{children}</div>
